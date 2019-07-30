8e947cab121978b2b3870c59768fa2be9d3c853d
Luis Dominguez <reynaldodominguez@gmail.com>
2019-07-29 21:20:05 -0400

    upcoming games and search

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,5 @@
 console.log('logic.js is connected');
+var teams = [];
 $(document).ready(function(){
     console.log('js connected');
   //Hides the leauge page on load
@@ -19,8 +20,25 @@ $(document).ready(function(){
     
     });
 
+    var leagues = ["nba", "nfl", "mlb", "American Major League Soccer"];
+    for(var i = 0; i < leagues.length; i++){
+      console.log(leagues[i]);
+    var queryURL = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=" + leagues[i];
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+      //console.log(response);
+      for(var j = 0; j < response.teams.length;j++){
+        //console.log(response.teams[j].strTeam);
+        teams.push(response.teams[j].strTeam);
+       
+      }  
+      
+    });
+    //console.log(teams);
+  }
     
-    
-   
-    
-    
+  console.log(teams);
